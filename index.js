dd9c9f74779fdda8d640e1d8d9c6e728ddd8d4f7
diomededavid <david@d2creativedesign.com>
2019-05-26 13:40:40 -0700

    Solution form Go Make things

--- index.js
+++ index.js
@@ -3,19 +3,39 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Word Count</h1>`;
 
-let textInput = document.querySelector('#text-input');
-let wordCount = document.querySelector('#word-count');
-let characterCount = document.querySelector('#character-count');
-
-
+let textArea =  document.querySelector('[data-character-count]')
+let words = document.querySelector('#word-count');
+let characters = document.querySelector('#character-count');
 
 
-textInput.addEventListener('keyup', function (event) {
-  if (textInput.value.length < 1) return;
-  
-  if (textInput.value) {
-    characterCount.innerHTML = textInput.value.length;
-  }
-});
+//  1. Create varables and selectors for elements
+//  2. Setup function whenever textarea changes 
+//  3. Make sure the event happened on textarea using matches()
+//  4. To get the word count. Create an array with each word from the textarea. Use split() method to turn a strin ginto an array based on some delimiter eg: space, comma, etc.
+
+var inputHandler = function (event) {
+  let wordCount = event.target.value.split(/[\n\r\s]+/g).filter(function(word){
+    return word.length > 0;
+  });
+
+  if (!event.target.matches('[data-character-count]')) return;
+    
+   words.textContent = wordCount.length;
+   characters.textContent = event.target.value.length;
+}
+
+document.addEventListener('input', inputHandler, false)
+
+
+
+
+// textArea.addEventListener('input', function (event) {
+//   if (!textArea > 1) return;
+
+//   if (textArea.value) {
+//     let wordCount 
+//     characters.innerHTML = textArea.value.length;
+//   }
+// });
